207f2ab9dbdefb0cdc4ceee0701c965b76e4944b
Panipak Jongsirivanich <panipak.jong@kmutt.ac.th>
2023-09-10 16:43:57 +0700

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,23 @@
+function fizzBuzz(max) {
+  
+  
+  let result = ''
+
+  for(let i = 1 ; i <= max ; i++){
+
+    if( i % 3 === 0 && i % 5 === 0) {
+      result += 'FizzBuzz'
+    }else if(i % 3 === 0) {
+      result +='Fizz'
+    }else if(i % 5 === 0 ) {
+      result += 'Buzz'
+    }else{
+      result += i
+    }
+  }
+
+  return result
+
+
+}
+module.exports = fizzBuzz
